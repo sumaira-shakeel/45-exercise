c578b423f8bdd001ac382197b26052bcf4721402
unknown <sumairasami2809@gmail.com>
2024-03-26 20:12:22 +0500

    first commit

---
+++ q_2/main.js
@@ -0,0 +1,4 @@
+//q#1
+let personName = 'Sumaira';
+console.log(`Hello ${personName} , would you like to learn some typescript today?`);
+export {};
---
+++ q_2/main.ts
@@ -0,0 +1,3 @@
+//q#1
+let personName:string = 'Sumaira';
+console.log(`Hello ${personName} , would you like to learn some typescript today?`);
